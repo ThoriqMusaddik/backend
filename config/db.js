e0ebe8c04bf6c827bc2b37569f7791537560f46b
ThoriqMusaddik <email_kamu@contoh.com>
2025-06-11 13:26:17 +0700

    data my sql

--- config/db.js
+++ config/db.js
@@ -1,15 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.MYSQLDATABASE,     // Nama database
-  process.env.MYSQLUSER,         // Username
-  process.env.MYSQLPASSWORD,     // Password
-  {
-    host: process.env.MYSQLHOST, // Host dari Railway
-    dialect: 'mysql',
-    port: process.env.MYSQLPORT || 3306, // Default 3306 jika tidak ada
-    logging: false, // Boleh true untuk debugging query
-  }
-);
+const sequelize = new Sequelize('db_pdfkita', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
 
 module.exports = sequelize;
