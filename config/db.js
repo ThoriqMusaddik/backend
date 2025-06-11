2e3bf303ee69c6eadfc3e1eeec1ad0dc58ca3d98
ThoriqMusaddik <email_kamu@contoh.com>
2025-06-12 03:50:20 +0700

    pembaruan

--- config/db.js
+++ config/db.js
@@ -1,13 +1,35 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'mysql',
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: true
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  // Jika pakai DATABASE_URL (Railway style)
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false, // <-- kunci penting biar self-signed certificate diterima
+      },
+    },
+  });
+} else {
+  // Jika pakai variabel lama
+  sequelize = new Sequelize(
+    process.env.MYSQLDATABASE,
+    process.env.MYSQLUSER,
+    process.env.MYSQLPASSWORD,
+    {
+      host: process.env.MYSQLHOST,
+      port: process.env.MYSQLPORT,
+      dialect: 'mysql',
+      dialectOptions: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
     }
-  }
-});
+  );
+}
 
 module.exports = sequelize;
